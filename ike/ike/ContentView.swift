de74a902e9857dc87b596f98a5e6049be50f77f9
GaetanoCelentano99 <gaetano99celentano@gmail.com>
2022-10-21 12:40:15 +0200

    made the interface, TODO: the logic behind all

--- ike/ike/ContentView.swift
+++ ike/ike/ContentView.swift
@@ -8,21 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    //getting the screen boundaries for sizing and placing
     let screenWidth = UIScreen.main.bounds.width
     let screenHeight = UIScreen.main.bounds.height
+    
+    
     var body: some View {
         VStack {
+            // horizontal stack for the actions------------------------------------------------------
             HStack{
-                Image(systemName: "plus")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
+                Button(action: {}) {
+                   Text("+").font(.system(size: 36))
+                }
+                .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: screenWidth*0.05))
             }
-            .frame(width: screenWidth,
-                   alignment: .trailing)
-           
+            .frame(width: screenWidth, alignment: .trailing)
+            .padding(EdgeInsets(top: screenHeight*0.05, leading: 0, bottom: 0, trailing: 0))
+            
+            
+            
+            //space reserved for the pie chart------------------------------------------------------
+            ZStack{
+                
+            }
+            .frame(width: screenWidth*0.65,height: screenWidth*0.65)
+            .background(.red)
+            
+            
+            //space for the priority switch------------------------------------------------------
+            HStack{
+                OrderChoices()
+            }
+            .frame(width: screenWidth*0.9)
+
+            // space for the task list------------------------------------------------------
+            VStack{
+                TaskList()
+                    .cornerRadius(10)
+            }
+            .frame(width: screenWidth*0.9)
+            .padding(EdgeInsets(top: 0, leading: 0, bottom: screenHeight*0.05, trailing: 0))
+            
+            
+            
         }
+        .frame(width: screenWidth, height: screenHeight, alignment: .top)
+        
+            
        
-        .padding()
         
     }
 }
--- ike/ike/ikeApp.swift
+++ ike/ike/ikeApp.swift
@@ -7,6 +7,68 @@
 
 import SwiftUI
 
+
+struct OrderChoice : Identifiable{
+    let id = UUID()
+    var value : String
+}
+let choices = [
+    OrderChoice(value: "Order by DeadLine"),
+    OrderChoice(value: "Order by Priority"),
+]
+
+struct OrderChoices : View {
+    @State var selected = choices[0].id
+    var body: some View{
+        Picker(selection: $selected, label: Text("Order Preference")) {
+            ForEach(choices) { choice in
+                Text(choice.value)
+                    .onTapGesture {
+                    selected = choice.id
+                }
+                    
+               }
+        }
+        .pickerStyle(SegmentedPickerStyle())
+        
+    }
+}
+
+
+struct Task : Identifiable{
+    let id = UUID()
+    var taskName : String
+    var deadline: String
+    var priority: String
+    //var not_freq: Notification
+    
+}
+
+
+var tasks = [
+    Task(taskName: "Study for the ADA", deadline: "26/10/2022", priority:"Schedule It"),
+    Task(taskName: "Study MAPI", deadline: "25/10/2022", priority:"Do It"),
+    Task(taskName: "Walk the dog", deadline: "25/10/2022", priority:"Delegate It"),
+    Task(taskName: "Do the laundry", deadline: "25/10/2022", priority:"Delete It"),
+]
+
+struct TaskList: View {
+    var body: some View {
+        
+        List {
+            ForEach(tasks) { task in
+                Text(task.taskName)
+                    .listRowBackground(task.priority == "Do It" ? Color.red.opacity(0.8) :
+                                        task.priority == "Schedule It" ? Color.orange.opacity(0.8) :
+                                        task.priority == "Delegate It" ? Color.green.opacity(0.7) :
+                                        task.priority == "Delete It" ? Color.yellow.opacity(0.7) : Color.gray)
+                }
+                    
+        }
+    }
+}
+
+
 @main
 struct ikeApp: App {
     var body: some Scene {
@@ -14,4 +76,5 @@ struct ikeApp: App {
             ContentView()
         }
     }
+
 }
